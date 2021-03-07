6f5acf71af7d030bdba55be9b9d7afa70ed1c00d
tfachmann <fachmanntj@gmail.com>
2021-03-07 15:43:01 +0100

    refactor(server): improved error handling / messages

--- snake_common/src/lib.rs
+++ snake_common/src/lib.rs
@@ -10,10 +10,27 @@ pub enum ClientMessage {
     SubmitName(Name, Score),
 }
 
+impl ClientMessage {
+    pub fn class_name(&self) -> &str {
+        match self {
+            ClientMessage::RequestHighscore(_) => "RequestHighscore",
+            ClientMessage::SubmitName(_, _) => "SubmitName",
+        }
+    }
+}
+
 #[derive(Debug, Deserialize, Serialize)]
 pub enum ServerMessage {
     Highscore {
         others: Vec<(Name, Score)>,
         you: (Position, Score),
+    }
+}
+
+impl ServerMessage {
+    pub fn class_name(&self) -> &str {
+        match self {
+            ServerMessage::Highscore { others: _, you: _ } => { "Highscore" }
+        }
     }
 }
--- snake_server/src/main.rs
+++ snake_server/src/main.rs
@@ -1,13 +1,14 @@
-use async_tungstenite::tungstenite::Message;
+use async_tungstenite::{tungstenite::Message, WebSocketStream};
 use env_logger::Env;
 use futures::{sink::SinkExt, stream::StreamExt};
-use log::{info, warn};
+use log::{debug, error, info, warn};
 use serde::{Deserialize, Serialize};
 use smol::Async;
 use snake_common::{ClientMessage, ServerMessage};
 use std::fs::File;
 use std::io::{Read, Write};
-use std::net::{SocketAddr, TcpListener};
+use std::net::{SocketAddr, TcpListener, TcpStream};
+use thiserror::Error;
 
 #[derive(Debug, Serialize, Deserialize)]
 struct HighScoreEntry {
@@ -16,8 +17,11 @@ struct HighScoreEntry {
 }
 
 impl HighScoreEntry {
-    fn new(name: String, score: u32) -> Self {
-        Self { name, score }
+    fn new(name: &str, score: u32) -> Self {
+        Self {
+            name: name.to_string(),
+            score,
+        }
     }
 }
 
@@ -25,21 +29,123 @@ fn read_highscore_list() -> Vec<HighScoreEntry> {
     match File::open("highscore_list") {
         Ok(mut file) => {
             let mut buffer = Vec::<u8>::new();
-            file.read_to_end(&mut buffer).unwrap();
-            bincode::deserialize(&buffer[..]).unwrap_or(vec![])
+            match file.read_to_end(&mut buffer) {
+                Ok(_) => match bincode::deserialize(&buffer[..]) {
+                    Ok(data) => data,
+                    Err(_) => {
+                        error!("Could not deserialize, creating new data");
+                        vec![]
+                    }
+                },
+                Err(e) => {
+                    error!("Could not read file content ({}), creating new data", e);
+                    vec![]
+                }
+            }
         }
         Err(_) => {
-            warn!("File doesn't exist yet, creating it...");
-            File::create("highscore_list").expect("Could not create file");
+            warn!("File doesn't exist yet, will create it at the next write...");
             vec![]
         }
     }
 }
 
-fn write_highscore_list(data: &Vec<HighScoreEntry>) {
-    let mut file = File::create("highscore_list").expect("could not open file");
-    let bytes = bincode::serialize(&data).expect("Could not serialize data");
-    file.write_all(&bytes).expect("Could not write file");
+#[derive(Error, Debug)]
+pub enum WriteError {
+    /// Represents a failure to read from input.
+    #[error("Serialization error")]
+    IFSError(#[from] std::boxed::Box<bincode::ErrorKind>),
+
+    /// Represents all other cases of `std::io::Error`.
+    #[error(transparent)]
+    IOError(#[from] std::io::Error),
+}
+
+fn write_highscore_list(data: &Vec<HighScoreEntry>) -> Result<(), WriteError> {
+    let mut file = File::create("highscore_list")?;
+    let bytes = bincode::serialize(&data)?;
+    file.write_all(&bytes)?;
+    Ok(())
+}
+
+async fn request_highscore(
+    score: u32,
+    stream: &mut WebSocketStream<Async<TcpStream>>,
+    highscore_vec: &mut Vec<HighScoreEntry>,
+) {
+    // return entries
+    let resp = ServerMessage::Highscore {
+        others: {
+            let slice = if highscore_vec.len() < 10 {
+                &highscore_vec
+            } else {
+                &highscore_vec[highscore_vec.len() - 10..]
+            };
+            slice
+                .iter()
+                .rev()
+                .map(|el| (el.name.clone(), el.score))
+                .collect()
+        },
+        you: {
+            let mut position = highscore_vec.len();
+            for (idx, el) in highscore_vec.iter().rev().enumerate() {
+                if score > el.score {
+                    position = idx;
+                    break;
+                }
+            }
+            (position as u32, score)
+        },
+    };
+    match bincode::serialize(&resp) {
+        Err(e) => error!(
+            "Could not serialize Highscore response {}, no message sent",
+            e
+        ),
+        Ok(encoded) => {
+            debug!("Sucessfully encoded message");
+            match stream.send(Message::Binary(encoded)).await {
+                Ok(_) => debug!("Sucessfully sent response message"),
+                Err(e) => error!("Could not send response: {}", e),
+            }
+        }
+    }
+}
+
+async fn submit_entry(name: String, score: u32, highscore_vec: &mut Vec<HighScoreEntry>) {
+    info!("Submitting Name: {} with score: {}", name, score);
+    highscore_vec.push(HighScoreEntry::new(&name, score));
+    highscore_vec.sort_by_key(|el| el.score);
+    match write_highscore_list(&highscore_vec) {
+        Ok(_) => info!("Sucessfully submitted {}", &name),
+        Err(e) => error!("Could not write file: {}", e),
+    }
+}
+
+async fn read_stream(
+    mut stream: WebSocketStream<Async<TcpStream>>,
+    highscore_vec: &mut Vec<HighScoreEntry>,
+) {
+    while let Some(Ok(Message::Binary(t))) = stream.next().await {
+        match bincode::deserialize::<ClientMessage>(&t) {
+            Err(e) => error!("Could not Deserialize msg: {}", e),
+            Ok(msg) => {
+                info!(
+                    "Received and deserialized msg of type `{}`",
+                    msg.class_name()
+                );
+                match msg {
+                    ClientMessage::RequestHighscore(score) => {
+                        request_highscore(score, &mut stream, highscore_vec).await
+                    }
+                    ClientMessage::SubmitName(name, score) => {
+                        submit_entry(name, score, highscore_vec).await
+                    }
+                }
+            }
+        }
+    }
 }
 
 pub fn main() {
@@ -47,7 +153,6 @@ pub fn main() {
     let addr = "127.0.0.1:8090";
 
     let mut highscore_vec: Vec<HighScoreEntry> = read_highscore_list();
-    println!("{:?}", highscore_vec);
 
     smol::block_on(async {
         info!("Listening on: {}", addr);
@@ -57,50 +162,13 @@ pub fn main() {
 
         while let Ok((stream, addr)) = listener.accept().await {
             info!("Got connection from {}", addr);
-            let mut ws_stream = async_tungstenite::accept_async(stream)
-                .await
-                .expect("Oh, could not get message");
-            while let Some(Ok(Message::Binary(t))) = ws_stream.next().await {
-                let msg =
-                    bincode::deserialize::<ClientMessage>(&t).expect("Could not deserialize msg");
-                match msg {
-                    ClientMessage::RequestHighscore(score) => {
-                        info!("Requesting Highscore: {}", score);
-                        // return entries
-                        let resp = ServerMessage::Highscore {
-                            others: {
-                                let slice = if highscore_vec.len() < 10 {
-                                    &highscore_vec
-                                }
-                                else {
-                                    &highscore_vec[highscore_vec.len() - 10..]
-                                };
-                                slice.iter().rev().map(|el| (el.name.clone(), el.score)).collect()
-                            },
-                            you: {
-                                let mut position = highscore_vec.len();
-                                for (idx, el) in highscore_vec.iter().rev().enumerate() {
-                                    if score > el.score {
-                                        position = idx;
-                                        break;
-                                    }
-                                }
-                                (position as u32, score)
-                            }
-                        };
-                        let encoded = bincode::serialize(&resp)
-                            .expect("Could not serialize Highscore response");
-                        ws_stream
-                            .send(Message::Binary(encoded))
-                            .await
-                            .expect("Highscore could not be delivered");
-                    }
-                    ClientMessage::SubmitName(name, score) => {
-                        info!("Submitting Name: {} with score: {}", name, score);
-                        highscore_vec.push(HighScoreEntry::new(name, score));
-                        highscore_vec.sort_by_key(|el| el.score);
-                        write_highscore_list(&highscore_vec);
-                    }
+            match async_tungstenite::accept_async(stream).await {
+                Err(e) => {
+                    error!("Could not get stream: {}", e);
+                }
+                Ok(ws_stream) => {
+                    info!("Reading incomming stream...");
+                    read_stream(ws_stream, &mut highscore_vec).await;
                 }
             }
         }
